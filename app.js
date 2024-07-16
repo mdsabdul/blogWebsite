5cd9cae8d757ab609c0a802314f72f45051bc790
mdsabdul <mdsabdul088@gmail.com>
2024-07-16 12:16:03 +0530

    updated

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ passport.deserializeUser(User.deserializeUser())
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
